26fb8a833dbd178639613d46607a43b0650f2725
darkcris1 <sircnujnuj@gmail.com>
2021-05-09 22:23:27 +0800

    refactor: simplified config

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,46 @@
-
-import { resolve } from "path";
 import legacy from "@vitejs/plugin-legacy";
 import glob from "glob";
 import autoprefixer from "autoprefixer";
 import uncss from "postcss-uncss";
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production" || null;
 
-const files = glob.sync("./src/pages/**/*.html");
-
-const getPath = (path = "",fileName = "index.html")=>{
-	return resolve(__dirname, `src/pages/${!path ? "" : path + "/"}${fileName}`);
-}
-
-const inputs = files.reduce((acc,val)=>{
-	val = val.replace("./src/pages/","");
-	const folders = val.split("/");
-	const isFolderIndexHtml = /index\.html$/.test(val);
-	const isFirstLevelFolder = folders.length > 1;
-	
-	if (val === "index.html"){
-		acc.main = getPath()
-	}else if ( isFirstLevelFolder){
-		const fileName = folders[folders.length - 1];
-		const folderPaths = folders.splice(0,folders.length - 1);
-		const dir = folderPaths.join("/");
-		
-		if (fileName !== "index.html"){
-			folderPaths.push(fileName.replace(/\.html$/,""))
-		}
-
-		acc[folderPaths.join(".")] = getPath(dir,fileName)
-	}
-	
-	return acc;
-},{})
+const buildFolder = "__build__";
+const assetsFolder = "___assets__";
+const isLegacy = false; // set True if you want to support older browsers or < ES2015
 
 export default {
-	build: {
-		rollupOptions: {
-			input: inputs,
-			output: {
-				entryFileNames: `assets/js/[name].js`,
-      		chunkFileNames: `assets/chunks/[name].js`,
-		      assetFileNames: (e)=>{
-	      		if (/\.css$/.test(e.name)) return "assets/css/[name].[hash].css"
-	
-	        		return "assets/[name].[hash].[ext]"
-		      }
-			}
-		},
-		outDir: "../../dist",
-		emptyOutDir: true,
-	},
-	// css: {
-	// 	postcss: {
-	// 		plugins: [
-				
-	// 				autoprefixer({ grid: "autoplace"}),
-	// 				uncss({ html: ["./src/pages/**/*.html"]})
-			
-	// 		]
-	// 	}
-	// },
-	root: "./src",
-	publicDir: "../public"
-}
+  root: "./src",
+  build: {
+    rollupOptions: {
+      input: glob.sync("./src/**/*.html"),
+      output: {
+        entryFileNames: `${buildFolder}/js/[name].js`,
+        chunkFileNames: `${buildFolder}/chunks/[name].js`,
+        assetFileNames: (e) => {
+          if (/\.css$/.test(e.name))
+            return `${buildFolder}/css/[name].[hash].css`;
+
+          return `${buildFolder}/images/[name].[hash].[ext]`;
+        },
+      },
+    },
+    outDir: "../dist",
+    assetsDir: assetsFolder,
+    emptyOutDir: true,
+  },
+  css: {
+    postcss: {
+      plugins: isProd && [
+        autoprefixer({ grid: "autoplace" }),
+        uncss({ html: ["./src/**/*.html"] }),
+      ],
+    },
+  },
+  plugins: [
+    isLegacy &&
+      legacy({
+        targets: ["defaults"],
+      }),
+  ],
+};
